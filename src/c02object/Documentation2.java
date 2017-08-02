870ad4b1f6aedd807fdaf7f078af4e565053ab8d
李德宇 <1181661905@qq.com>
2017-08-02 17:01:54 +0800

    第一张 对象

---
+++ src/c02object/Documentation2.java
@@ -0,0 +1,9 @@
+package c02object;
+
+/**
+ * <pre>
+ * System.out.println(new Date());
+ * </pre>
+ */
+public class Documentation2 {
+}
---
+++ src/c02object/ShowProperties.java
@@ -0,0 +1,10 @@
+package c02object;
+
+public class ShowProperties {
+	public static void main(String[] args) {
+		// 输出所有系统属性
+		System.getProperties().list(System.out);
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("java.library.path"));
+	}
+}

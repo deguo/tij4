151b5b13807dd881b9e7fbfdc080514e046e1327
李德宇 <1181661905@qq.com>
2017-08-10 19:54:02 +0800

    第四章 控制执行流程

---
+++ src/ch04control/ForEachFloat.java
@@ -0,0 +1,29 @@
+package ch04control;
+
+import java.util.*;
+
+/**
+ * <pre>
+ * Output:
+ * 0.72711575
+ * 0.39982635
+ * 0.5309454
+ * 0.0534122
+ * 0.16020656
+ * 0.57799757
+ * 0.18847865
+ * 0.4170137
+ * 0.51660204
+ * 0.73734957
+ * </pre>
+ */
+public class ForEachFloat {
+	public static void main(String[] args) {
+		Random rand = new Random(47);
+		float f[] = new float[10];
+		for (int i = 0; i < 10; i++)
+			f[i] = rand.nextFloat();
+		for (float x : f)
+			System.out.println(x);
+	}
+}

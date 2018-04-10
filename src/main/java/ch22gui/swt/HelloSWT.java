27c09494b2bc0b3848a2f7ce498a4d955e7d5613
lidy <lidy@szkingdom.com>
2018-04-10 18:32:42 +0800

    第22章gui

---
+++ src/main/java/ch22gui/swt/HelloSWT.java
@@ -0,0 +1,20 @@
+package ch22gui.swt;
+
+import org.eclipse.swt.widgets.*;
+
+/**
+ * {Requires: org.eclipse.swt.widgets.Display; You must
+ * install the SWT library from http://www.eclipse.org }
+ */
+public class HelloSWT {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Hi there, SWT!"); // Title bar
+        shell.open();
+        while (!shell.isDisposed())
+            if (!display.readAndDispatch())
+                display.sleep();
+        display.dispose();
+    }
+}
---
+++ src/main/java/ch22gui/swt/util/SWTApplication.java
@@ -0,0 +1,7 @@
+package ch22gui.swt.util;
+
+import org.eclipse.swt.widgets.*;
+
+public interface SWTApplication {
+    void createContents(Composite parent);
+}
